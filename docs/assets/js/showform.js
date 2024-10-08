0684ba603aec22e7d6a9466711f06164090c0dca
NTSil385 <tramv130597@gmail.com>
2024-10-08 09:04:30 +0700

    done login - register page

--- docs/assets/js/showform.js
+++ docs/assets/js/showform.js
@@ -1,24 +1,3 @@
-function showPopup() {
-    document.getElementById("popupForm").style.display = "block";
-    document.body.classList.add("no-scroll");
-    document.getElementById("popupFormRegister").style.display = "none";
-}
-
-function hidePopup() {
-    document.getElementById("popupForm").style.display = "none";
-    document.body.classList.remove("no-scroll");
-}
-
-function showRegister() {
-    document.getElementById("popupFormRegister").style.display = "block";
-    document.body.classList.add("no-scroll");
-    document.getElementById("popupForm").style.display = "none";
-}
-
-function hideRegister() {
-    document.getElementById("popupFormRegister").style.display = "none";
-    document.body.classList.remove("no-scroll");
-}
 
 function openFormXLCHT600() {
     document.getElementById("myForm").style.display = "block";
